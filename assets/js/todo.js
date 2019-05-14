4839a8a603d3f6eda9eb6889ba4a80d9f06de775
c376W <mailfzc123@gmail.com>
2019-05-14 12:33:06 -0700

    More styling relating lists.

--- assets/js/todo.js
+++ assets/js/todo.js
@@ -13,7 +13,7 @@ $("input[type='text']").keypress(function(event){
 	if(event.which===13){
 		var todo=$(this).val();
 		$(this).val("");
-		$("ul").append("<li><span>X</span> "+todo+"</li>");
+		$("ul").append("<li><span><i class='fas fa-trash-alt'></i></span> "+todo+"</li>");
 	}
 	
 })
