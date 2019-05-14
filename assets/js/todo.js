f5d28d0fd7c8128ac41f3326bc8119bd7952908b
c376W <mailfzc123@gmail.com>
2019-05-14 11:06:41 -0700

    Adding function for adding new item on the list.

--- assets/js/todo.js
+++ assets/js/todo.js
@@ -1,10 +1,19 @@
-$("li").click(function(){
+$("ul").on("click","li",function(){
 	$(this).toggleClass("done");
 })
 
-$("span").click(function(event){
+$("ul").on("click","span",function(event){
 	$(this).parent().fadeOut(500,function(){
 		$(this).remove();
 	})
 	event.stopPropagation();
+})
+
+$("input[type='text']").keypress(function(event){
+	if(event.which===13){
+		var todo=$(this).val();
+		$(this).val("");
+		$("ul").append("<li><span>X</span> "+todo+"</li>");
+	}
+	
 })
